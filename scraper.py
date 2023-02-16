b76c2b0152e1948fee6cf4e8f39171b4b7753954
Lucian D. Crainic <crainic.lucian@gmail.com>
2023-02-16 02:07:35 +0100

    Add json implementation for saving the data and json data inside directory

--- scraper.py
+++ scraper.py
@@ -1,35 +1,33 @@
-import datetime
+from datetime import datetime
+import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
+import json
+import base64
 
-'''
-Start the sample test.
-NOTE: This function needs to be implemented.
-'''
+#TODO implement this function for the sample test, where the questions are 25 for each try.
 def start_sample_test():
     pass
 '''
 Get all the data from the exam page.
+NOTE: this function is not used anymore, but it's still here for reference. You can use it if you want to download the exams via txt files.
 '''
-def get_data(driver,x):
-    # Create the exam directory
+def get_data_files(driver,x):
     directory = f"Exam_{x}"
     os.mkdir(directory)
     directory = f"Exam_{x}/Questions"
     os.mkdir(directory)
-
-    # Create the directories
+        
     for i in range(50):
         directory = f"Exam_{x}/Questions/Question_{i}"
         os.mkdir(directory)
         directory = f"Exam_{x}/Questions/Question_{i}/Choices"
         os.mkdir(directory)
 
-    # Get all the questions
     questions = driver.find_elements(by=By.XPATH, value='//div[@class="qtext"]')
     for i, question in enumerate(questions):
         directory = f"Exam_{x}/Questions/Question_{i}/Question"
@@ -46,7 +44,6 @@ def get_data(driver,x):
             with open(file_name, 'w') as file:
                 file.write(question.text)
 
-    # Get the choices
     domande = driver.find_elements(by=By.XPATH, value='//div[@class="answer"]//div[@class="d-flex w-100"]')
     contatore = 0
     numero = 0
@@ -64,8 +61,7 @@ def get_data(driver,x):
             file_name = f"Exam_{x}/Questions/Question_{numero}/Choices/choice_{contatore}.txt"
             with open(file_name, 'w') as file:
                 file.write(domanda.text)
-        
-    # Get the right answers
+
     risposte = driver.find_elements(by=By.XPATH, value='//div[@class="rightanswer"]')
     for i,risposta in enumerate(risposte):
         directory = f"Exam_{x}/Questions/Question_{i}/Answer"
@@ -79,9 +75,76 @@ def get_data(driver,x):
             with open(file_name, 'w') as file:
                 file.write(risposta.text)  
 '''
+Get all the data from the exam page.
+NOTE: this function is not used anymore, but it's still here for reference.
+'''
+def get_data_new(driver, numero_esame, directory_finale):
+    esame = { x:{} for x in range(50)}
+
+    questions = driver.find_elements(by=By.XPATH, value='//div[@class="qtext"]')
+    directory = f"Foto"
+    os.mkdir(directory)
+    for i, question in enumerate(questions):
+        if len(question.find_elements(by=By.TAG_NAME, value="img")) > 0:
+            file_name = f"Foto/answer_{i}.png"
+            with open(file_name, 'wb') as file:
+                file.write(question.find_element(by=By.TAG_NAME, value="img").screenshot_as_png)
+            with open(file_name, mode='rb') as old_file:
+                img = old_file.read()
+            esame[i]["domanda-immagine"] = base64.encodebytes(img).decode('utf-8')
+            esame[i]["domanda"] = question.text
+        else:
+            esame[i]["domanda"] = question.text
+    shutil.rmtree(directory)
+
+    domande = driver.find_elements(by=By.XPATH, value='//div[@class="answer"]//div[@class="d-flex w-100"]')
+    contatore = 0
+    numero = 0
+    directory = f"Foto"
+    os.mkdir(directory)
+    risposte = []
+    for domanda in domande:
+        if len(domanda.find_elements(by=By.TAG_NAME, value="img")) > 0:
+            contatore +=1
+            file_name = f"Foto/answer_{i}.png"
+            with open(file_name, 'wb') as file:
+                file.write(domanda.find_element(by=By.TAG_NAME, value="img").screenshot_as_png)
+            with open(file_name, mode='rb') as old_file:
+                img = old_file.read()
+            risposte.append(base64.encodebytes(img).decode('utf-8'))
+        else:
+            contatore+=1
+            risposte.append(domanda.text)
+        if contatore == 3:
+            esame[numero]["scelte"] = risposte
+            numero += 1
+            contatore = 0
+            risposte = []
+    shutil.rmtree(directory)
+    
+    risposte = driver.find_elements(by=By.XPATH, value='//div[@class="rightanswer"]')
+    directory = f"Foto"
+    os.mkdir(directory)
+    for i,risposta in enumerate(risposte):
+        if len(risposta.find_elements(by=By.TAG_NAME, value="img")) > 0:
+            file_name = f"Foto/answer_{i}.png"
+            with open(file_name, 'wb') as file:
+                file.write(risposta.find_element(by=By.TAG_NAME, value="img").screenshot_as_png)
+            with open(file_name, mode='rb') as old_file:
+                img = old_file.read()
+            esame[i]["risposta-immagine"] = base64.encodebytes(img).decode('utf-8')
+        else:
+            esame[i]["risposta"] = risposta.text
+    shutil.rmtree(directory)
+
+    t = f"Exam_{numero_esame}.json"
+    file_json = directory_finale + "/" + t
+    with open(file_json, mode='w') as file:
+        json.dump(esame, file, indent=4)
+'''
 Start a new exam. It's the same function of review_old_exam() but it starts the exam.
 '''
-def start_new_exam(driver, course_link, exam_link,i):
+def start_new_exam(driver, course_link, exam_link,i, directory):
     driver.get(course_link)
     driver.get(exam_link)
             
@@ -110,7 +173,7 @@ def start_new_exam(driver, course_link, exam_link,i):
     visualizza.click()
 
     time.sleep(3)
-    get_data(driver,i)
+    get_data_new(driver,i, directory)
 '''
 Review the old exams. It's the same function of start_new_exam() but it doesn't start the exam.
 '''
@@ -124,13 +187,12 @@ def review_old_exam(driver, course_link, exam_link,i):
     domande=driver.find_element("link text", "Show all questions on one page")
     domande.click()
     time.sleep(3)
-    get_data(driver,i)
+    get_data_new(driver,i)
 '''
 Login function for the elearning website. After login it starts scraping the exams inside the exams.txt file.
 NOTE: Insert your username and password in the send_keys() function.
 '''
-def login(driver):
-    # Login inside the website
+def login(driver, course_link, exam_link, i,directory):
     driver.get('https://elearning.uniroma1.it/auth/mtsaml/')
     username = driver.find_element(by=By.NAME, value="j_username")
     password = driver.find_element(by=By.NAME, value="j_password")
@@ -138,27 +200,28 @@ def login(driver):
     password.send_keys()
     login_button = driver.find_element(by=By.NAME, value="_eventId_proceed")
     login_button.click()
-    course_link = "https://elearning.uniroma1.it/course/view.php?id=11834" 
-    # Get all the exams links and either start a new exam or review an old one.
-    with open("exams.txt", "r") as file:
-        for i,line in enumerate(file):
-            exam_link = line.strip()
-            # Start a new exam and scrape the data
-            time.sleep(10)
-            start_new_exam(driver, course_link, exam_link, i)
-            print("Exam at linl " + exam_link + " scraped.")
-            # Review an old exam and scrape the data
-            # review_old_exam(driver, course_link, exam_link, i)
-    # open the folder with the exams and iterate over the files and open them
-
+    start_new_exam(driver, course_link, exam_link, i, directory)
 ''' 
 Main function of the program for running the scraper.
 NOTE : The path to the chromedriver must be changed to the path of the chromedriver on your computer.
 '''
 def main():
-    service = Service('/Users/lucian/Documents/chromedriver/chromedriver')
-    driver = webdriver.Chrome(service=service)
-    login(driver) 
+    now = datetime.now()
+    directory = now.strftime("%H:%M:%S")
+    os.mkdir(directory)
+    with open("exams.txt", "r") as file:
+        for i,line in enumerate(file):
+            service = Service('/Users/lucian/Documents/chromedriver/chromedriver')
+            course_link = "https://elearning.uniroma1.it/course/view.php?id=11834" 
+            driver = webdriver.Chrome(service=service)
+            exam_link = line.strip()
+            time.sleep(10)
+            login(driver, course_link, exam_link, i, directory)
 # Run the main function
 if __name__ == "__main__":
     main()
+    #TODO => Scraper stops after 7th exam. Fix it.
+        #? Manually pass the exam link for each exam and then run the scraper.
+        #! Check if the exam link is corrupted.
+        #? Try running the scraper with exams from 8th to ... and see if it works.
+    pass
